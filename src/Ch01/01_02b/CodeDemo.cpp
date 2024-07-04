cf59060d918971c6abbb6485cce4b6295a74aa97
mregusa <114294486+mregusa@users.noreply.github.com>
2024-07-04 22:37:37 +0000

    Changes

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Learning C++ 
 // Exercise 01_02
-// Hello World, by Eduardo Corpeño 
+// Hello World, by Miguel Regules
+
+#include <iostream>
+
+int main(){
+    std::cout << "Come caca Coca" << std::endl;
+    std::cout << std::endl << std::endl << std::endl;
+
+    return(0);
+}
